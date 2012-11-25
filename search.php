09b42caaa3159adaad9f06995bab4bdd231c4343
Nicole <nsliwa@smu.edu>
2012-11-25 08:05:31 -0600

    added advanced search with following format: #tag:tag_content #tag:tag_content...

--- search.php
+++ search.php
@@ -4,22 +4,55 @@
 	connectToDb();
 	if(isset($_POST['query']))
 	{
-		//echo '<p>'.$_POST['query'].'</p>';
-		$search_query = (explode(" ",$_POST['query']));
-		
 		$query = "SELECT id, path from photos WHERE";
 
-		foreach ($search_query as $key => $q) {
-			$query = $query . " (id IN (SELECT p.id FROM photos p JOIN tags t"
-			." WHERE p.id = t.photo_id AND (t.text LIKE '%".$q."%' OR p.title LIKE '%".$q."%')"
-			." GROUP BY p.id"
-			." ORDER BY count(p.id) desc))";
-			if(end($search_query) !== $q){
-			    $query = $query . " OR ";
-			}
+		if($_POST['query'][0] == '#')
+		{
+			$adv_search = $_POST['query'];
+			$adv_search = substr($adv_search, 1);
+			$search_tag = (explode("#", $adv_search));
+			foreach ($search_tag as $index => $tags) {
+				$search_seg = (explode(":", $tags));
+				$tag = $search_seg[0];
+				$search_query = (explode(" ", $search_seg[1]));
 
-			//$query = "select DISTINCT p.id, p.path from photos p JOIN tags t WHERE p.id = t.photo_id AND (t.text LIKE '%".$q."%' OR p.title LIKE '%".$q."%');";
-			//echo '<p>~~~~~~~~~~~~~~~~~~~~~~~~~~~</p>';
+				foreach ($search_query as $key => $q) {
+					//echo '<p>adv: '.$adv_search.' s_tag: '.$search_tag.' s_seg: '.$search_seg.' tag: '.$tag.' q:'.$q.' </p>';
+					$query = $query . " (id IN (SELECT p.id FROM photos p JOIN tags t"
+					." WHERE p.id = t.photo_id AND t.type = '". $tag ."' AND (t.text LIKE '%".$q."%' OR p.title LIKE '%".$q."%')"
+					." GROUP BY p.id"
+					." ORDER BY count(p.id) desc))";
+					if(end($search_query) !== $q){
+					    $query = $query . " OR ";
+					}
+					//echo '<br/><br/>' . $query . '<br/><br/>';
+
+					//$query = "select DISTINCT p.id, p.path from photos p JOIN tags t WHERE p.id = t.photo_id AND (t.text LIKE '%".$q."%' OR p.title LIKE '%".$q."%');";
+					//echo '<p>~~~~~~~~~~~~~~~~~~~~~~~~~~~</p>';
+				}
+				if(end($search_tag) !== $tags){
+					    $query = $query . " OR ";
+				}
+			}
+		
+		}
+		else
+		{
+			//echo '<p>'.$_POST['query'].'</p>';
+			$search_query = (explode(" ",$_POST['query']));
+
+			foreach ($search_query as $key => $q) {
+				$query = $query . " (id IN (SELECT p.id FROM photos p JOIN tags t"
+				." WHERE p.id = t.photo_id AND (t.text LIKE '%".$q."%' OR p.title LIKE '%".$q."%')"
+				." GROUP BY p.id"
+				." ORDER BY count(p.id) desc))";
+				if(end($search_query) !== $q){
+				    $query = $query . " OR ";
+				}
+
+				//$query = "select DISTINCT p.id, p.path from photos p JOIN tags t WHERE p.id = t.photo_id AND (t.text LIKE '%".$q."%' OR p.title LIKE '%".$q."%');";
+				//echo '<p>~~~~~~~~~~~~~~~~~~~~~~~~~~~</p>';
+			}
 		}
 
 		$query = $query . " GROUP BY id ORDER BY views desc;";
