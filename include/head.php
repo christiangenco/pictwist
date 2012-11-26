703a1892b35c3784b931ca6bb1819fc32760098c
Nicole <nsliwa@smu.edu>
2012-11-26 16:52:00 -0600

    lightbox stuff

--- include/head.php
+++ include/head.php
@@ -145,12 +145,51 @@
 <!DOCTYPE html>
 <head>
 <title>PicTwist</title>
-	<link href="styles/styles.css" rel="stylesheet" type="text/css">
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>	
+  <link href="styles/styles.css" rel="stylesheet" type="text/css">
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script> 
   <script src="js/jquery-ui-1.9.2.custom.min.js"></script> 
-	<script type="text/javascript" src="js/script.js"></script>
-	<link href="metro/css/m-styles.min.css" rel="stylesheet" />
-	<link rel="shortcut icon" href="img/icon.ico" />
+  <script type="text/javascript" src="js/script.js"></script>
+  <link href="metro/css/m-styles.min.css" rel="stylesheet" />
+  <link rel="shortcut icon" href="img/icon.ico" />
+
+  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+  <script type="text/javascript" src="fancyBox/source/jquery.fancybox.pack.js?v=2.1.3"></script>
+  <script type="text/javascript" src="js/script.js"></script>
+  <link rel="stylesheet" href="fancyBox/source/jquery.fancybox.css?v=2.1.3" type="text/css" media="screen" />
+  <link href="styles/styles.css" rel="stylesheet" type="text/css">
+
+  <script type="text/javascript">
+    $(document).ready(function() {
+      //alert("Hey there");
+      $(".fancybox").fancybox();
+      
+      $(".fancybox-iframe").fancybox({
+        
+        type : 'iframe',
+        prevEffect : 'fade',
+        nextEffect : 'fade',
+        openEffect : 'none',
+        closeEffect : 'none',
+        margin : [20, 60, 20, 60],        
+        
+        closeBtn : true,
+        arrows : true,
+        nextClick : false,
+        
+        helpers: {
+          title : {
+            type : 'inside'
+          }
+        },
+        
+        beforeShow: function() {
+          this.width = 1000;
+        }
+        
+      });
+      //alert("leaving...");
+    });
+  </script>
 </head>
 <body>
 	
