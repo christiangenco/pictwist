eb1cbd9cdc44801533244689cf682d497fe4e527
Christian Genco <christian.genco@gmail.com>
2012-11-26 18:46:25 -0600

    split js and css includes into their own file so they can be included seperately

---
+++ include/cssAndJsIncludes.php
@@ -0,0 +1,5 @@
+<link href="styles/styles.css" rel="stylesheet" type="text/css">
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script> 
+<script src="js/jquery-ui-1.9.2.custom.min.js"></script> 
+<script type="text/javascript" src="js/script.js"></script>
+<link href="metro/css/m-styles.min.css" rel="stylesheet" />
--- include/headLayout.php
+++ include/headLayout.php
@@ -1,11 +1,7 @@
 <!DOCTYPE html>
 <head>
 <title>PicTwist</title>
-  <link href="styles/styles.css" rel="stylesheet" type="text/css">
-  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script> 
-  <script src="js/jquery-ui-1.9.2.custom.min.js"></script> 
-  <script type="text/javascript" src="js/script.js"></script>
-  <link href="metro/css/m-styles.min.css" rel="stylesheet" />
+  <?php INCLUDE 'include/cssAndJsIncludes.php' ?>
   <link rel="shortcut icon" href="img/icon.ico" />
 </head>
 <body>
