6fc1ca410f666b808e19e330981fc1494009af01
Maryssa Crews <mecrews@smu.edu>
2012-12-02 08:57:57 -0600

    Removed extra links on login.php, removed list of photos on profile.php, changed name of first album to Default on register.processor.php

--- login.php
+++ login.php
@@ -3,19 +3,19 @@
 <html>
 	<head>
 		<title>Login</title>
-		<style type="text/css">
+		<!--<style type="text/css">
 			ul{list-style-type:none; margin:0; padding:0; background-color:blue; padding:5px;}
 			li{display:inline; color:white; padding:0px 50px 0px 10px;}
 			li a{color:white;}
-		</style>
+		</style>-->
 	</head>
 	
 	<body>
-		<ul>
+		<!--<ul>
 			<li><a href="<?php echo $searchURL ?>">Search Photos</a></li>
 			<li style="float:right;"><a href="<?php echo $loginURL ?>">Login</a></li>
 			<li style="float:left;"><a href="<?php echo $registerURL ?>">Register</a></li>
-		</ul>
+		</ul>-->
 		<form id="Login" action="<?php echo $loginHandlerURL ?>" enctype="multipart/form-data" method="post">
 			<h1>
 				Login
